3b9d9edf59e908f8ca1cd844a128317883846729
nexorqk <pkms2a10@gmail.com>
2025-02-20 01:03:19 +0300

    feat: add merge objects implementation

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,16 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const res = {};
+
+  objects.forEach((item) => {
+    Object.entries(item).forEach((entr) => {
+      res[entr[0]] = (res[entr[0]] || 0) + entr[1];
+    });
+  });
+
+  return res;
 }
 
 /**
