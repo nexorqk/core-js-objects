80fef3fff93497e2eda006b9097cb353a2d8bc88
nexorqk <pkms2a10@gmail.com>
2025-02-20 01:16:01 +0300

    feat: add remove properties solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -58,8 +58,16 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = { ...obj };
+
+  keys.forEach((key) => {
+    if (obj[key]) {
+      delete newObj[key];
+    }
+  });
+
+  return newObj;
 }
 
 /**
